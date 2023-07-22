2f471be57fc00d9a614a81f3b9e930cdf3c83762
pc1 <mohammadranjbar.mr81@gmail.com>
2023-07-22 10:24:56 +0330

    changes

--- src/main/java/com/example/Security/config/securityConfig.java
+++ src/main/java/com/example/Security/config/securityConfig.java
@@ -14,14 +14,14 @@ public class securityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-        .authorizeRequests()
-        .antMatchers("/","/login").permitAll()
-        .anyRequest().authenticated()
-        .and()
-        .formLogin().loginPage("/login").usernameParameter("email").defaultSuccessUrl("/index");
+                .authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").usernameParameter("email").defaultSuccessUrl("/index");  
     }
 
-    @Override
+    @Override 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
         .withUser("Mohammad")
